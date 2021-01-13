3abd92b7ecc098508f67ada9255e7107f046c02a
Priya-Salke <77397759+Priya-Salke@users.noreply.github.com>
2021-01-13 20:06:17 +0530

    Add files via upload

---
+++ EncapDemo1.java
@@ -0,0 +1,14 @@
+
+public class EncapDemo1 
+{
+	public static void main(String[] args)
+	{
+		Employee e1 = new Employee();
+		e1.setName("Samba");
+		e1.setAge(29);
+		e1.setId(44);
+		System.out.println("name: "+e1.getName());
+		System.out.println("age: "+e1.getAge());
+	}
+
+}
